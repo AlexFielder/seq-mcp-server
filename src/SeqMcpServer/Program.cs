0394b6145bce831be7f04ed55f57987a50bd2449
Alex Fielder <alex.fielder@gmail.com>
2026-03-17 16:25:24 +0000

    Bump default SeqVersion Max to 2025.3
    Update Program.cs to change the fallback Seq version maximum from 2025.2 to 2025.3. This adjusts the default used during startup validation when SeqVersion:Max is not configured, allowing compatibility with Seq 2025.3.

--- src/SeqMcpServer/Program.cs
+++ src/SeqMcpServer/Program.cs
@@ -162,7 +162,7 @@ var host = builder.Build();
 
 // Seq version validation on startup (without logging since we can't write to console)
 var minVer = Version.Parse(builder.Configuration["SeqVersion:Min"] ?? "2024.1");
-var maxVer = Version.Parse(builder.Configuration["SeqVersion:Max"] ?? "2025.2");
+var maxVer = Version.Parse(builder.Configuration["SeqVersion:Max"] ?? "2025.3");
 
 var logger = host.Services.GetService<ILogger<Program>>();
 
